76898e06999832d1483a4d62c67c7929cb023ee1
Mark A. Stratman <stratman@gmail.com>
2020-02-11 22:48:29 -0600

    converted line endings for more files

--- bin/fix-line-endings.sh
+++ bin/fix-line-endings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in static/files/*.spn static/files/*/*.spn; do
+for file in `find . -type f -name "*.spn"`; do
   file $file | grep -v CRLF
   if [ $? == 0 ]; then
     perl -pi -e 's/\n/\r\n/' $file
